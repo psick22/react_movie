8c80e32cd8c47a9983848bfab336fb9749c44b8d
psick22 <kgirok@gmail.com>
2020-12-29 00:12:50 +0900

    Study about state & class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h2>
-        I like {name} {image}
-      </h2>
-      <h3>{rating}/5.0</h3>
-    </div>
-  );
-}
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const foodILike = [
-  { id: 1, name: "egg", image: "asdfasdf", rating: 5 },
-  { id: 2, name: "sushi", image: "asdfasdf", rating: 3 },
-  { id: 3, name: "bread", image: "asdfasdf", rating: 4 },
-];
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((food) => {
-        return (
-          <Food
-            key={food.id}
-            name={food.name}
-            image={food.image}
-            rating={food.rating}
-          />
-        );
-      })}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>number is {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
